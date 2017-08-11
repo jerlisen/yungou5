b64d39a06dcb5c47add1620508f67b46bf4f4d73
shandong115 <mr.danzhao@163.com>
2017-08-11 18:15:02 +0800

    modify domain.inc.php, and add iphone7 plus picture

--- system/config/domain.inc.php
+++ system/config/domain.inc.php
@@ -1,12 +1,6 @@
 <?php 
 
 return array (
-  'm.demo.52jscn.com' => 
-  array (
-    'm' => 'mobile',
-    'c' => 'mobile',
-    'a' => 'init',
-  ),
   'm.1ytb.com' => 
   array (
     'm' => 'mobile',
